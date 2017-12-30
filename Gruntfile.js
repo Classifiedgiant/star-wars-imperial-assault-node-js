6538fec016aa67b5f7c99e4572c08d5c9e8c3b6c
Thomas Lockie <tclockie@gmail.com>
2017-12-30 16:45:40 +0000

    * Added grunt usage from VS code and deploying to local web server

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function(grunt)
 {
 	grunt.initConfig(
 	{
+		globalConfig: {
+			serverDir: "D:/xampp/htdocs/stia"
+		},
+	
 		pkg: grunt.file.readJSON("package.json"),
 		jshint:
 		{
@@ -28,13 +32,24 @@ module.exports = function(grunt)
 					}
 				}
 			}
-
+		},
+		copy : {
+			main: {
+				options: {
+					banner: "/*! <%= globalConfig.serverDir %> */\n"
+				},
+				files: [
+					{expand: true, src: ["./build/**"], dest: "<%= globalConfig.serverDir %>/"},
+					{src: "./index.html", dest: '<%= globalConfig.serverDir %>/index.html'}
+				]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-jshint")
+	grunt.loadNpmTasks("grunt-contrib-copy")
 	grunt.loadNpmTasks("grunt-browserify")
 
 	grunt.registerTask("default", ["jshint","browserify"])
-
+	grunt.registerTask("deploy", ["jshint", "browserify", "copy:main"])
 };
